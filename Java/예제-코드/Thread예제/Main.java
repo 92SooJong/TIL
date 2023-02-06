5a2b9d96bd10d8d5c042e65abc7fe55628a1c7e0
하수종 <hsz3855@gmail.com>
2023-02-06 10:18:11 +0900

    modify directory

---
+++ Java/예제-코드/Thread예제/Main.java
@@ -0,0 +1,39 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        // 티켓시스템 생성
+        TicketSystem ticketSystem = new TicketSystem(0);
+
+        // 스탭 쓰레드 1개 생성
+        Staff staff = new Staff(ticketSystem);
+        staff.start();
+
+        // Customer의 인스턴스를 담을 List
+        ThreadGroup customerGroup = new ThreadGroup("customerGroup");
+        // 10개 고객 쓰레드 생성과 시작
+        for(int i=0; i<10; i++){
+            String customerName = "customer".concat( String.valueOf(i)); // 쓰레드 이름 생성
+            Customer customer = new Customer(ticketSystem,customerGroup , customerName); // 고객쓰레드 생성
+            customer.start();
+        }
+
+
+        // 고객 쓰레드가 모두 종료된 경우 스탭 쓰레드를 종료한다.
+        while(true){
+            // customerGroup내 활성화된 쓰레드가 0개 인경우
+            if(customerGroup.activeCount() == 0){
+                staff.setWorkDone(true);
+                staff.join();
+                break;
+            }
+        }
+
+
+
+    }
+}
